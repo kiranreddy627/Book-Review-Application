5d32c60c8a51343532dff007b916ccd5640e4502
Omkiran Reddy Bojja <99016756+kiranreddy627@users.noreply.github.com>
2024-11-20 18:56:06 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,6 @@ const bookRoutes = require("./routes/bookRoutes");
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
-app.use(cors());
 
 
 const uri = process.env.ATLAS_URI;
